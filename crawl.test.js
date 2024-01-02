c8c13f972ffbd1ca0a8fc52a192fcd2574ce4d7a
Ibezimmichael <ibezim.michael@yahoo.com>
2024-01-02 15:14:27 +0100

    test function established

--- crawl.test.js
+++ crawl.test.js
@@ -10,8 +10,24 @@ test('normalizeUrl strip protocol', () => {
     expect(actual).toEqual(expected)
 })
 
-test('normalizeUrl strip protocol', () => {
+test('normalizeUrl strip trailing slash', () => {
     const input = "https://blog.boot.dev/path/";
+    const actual = normalizeUrl(input);
+    const expected = "blog.boot.dev/path";
+
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeUrl capitals', () => {
+    const input = "https://BLOG.boot.dev/path/";
+    const actual = normalizeUrl(input);
+    const expected = "blog.boot.dev/path";
+
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeUrl protocol', () => {
+    const input = "http://BLOG.boot.dev/path/";
     const actual = normalizeUrl(input);
     const expected = "blog.boot.dev/path";
 
