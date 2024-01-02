c5fc1c2c3671015c44c68e04176b73f36c771115
Ibezimmichael <ibezim.michael@yahoo.com>
2024-01-02 17:02:04 +0100

    function to crawl single page

--- crawl.js
+++ crawl.js
@@ -1,4 +1,27 @@
+const { log } = require('console');
 const {JSDOM} = require('jsdom');
+
+async function crawlPage(currentURl){
+    console.log("craling now");
+    try{
+        const resp = await fetch(currentURl)
+        if (resp.status > 399) {
+            console.log(`error in fetch with status code ${resp.status} on page ${currentURl}`);
+            return
+        }
+        const contentType = resp.headers.get("content-type")
+        if (!contentType.includes("text/html")) {
+            console.log(`Not html response, content: ${contentType} on page ${currentURl}`);
+            return
+            
+        }
+        console.log(await resp.text());
+    }catch (err){
+        console.log(`error in fetch: ${err.message}, on page ${currentURl}`);
+    }
+
+
+}
 
 function getURLsFromHTMl(html, baseURL){
     const urls = []
@@ -37,5 +60,6 @@ function normalizeUrl(url){
 
 module.exports = {
     normalizeUrl,
-    getURLsFromHTMl
+    getURLsFromHTMl,
+    crawlPage
 }
